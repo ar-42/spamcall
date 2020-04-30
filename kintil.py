0904daf632826548ad78ae76f0b297f8d621e757
storiku <50206225+storiku@users.noreply.github.com>
2020-04-30 08:40:38 +0700

    Delete kintil.py

--- kintil.py
+++
@@ -1,9 +0,0 @@
-import os,sys,time
-import requests
-import subprocess
-from requests import post
-
-#Penggunaan lihat di website berikut
-# https://omcyber.com/spam-call-termux
-
-url = "https://id.jagreward.com/member/verify-mobile/nomerdisini/"
